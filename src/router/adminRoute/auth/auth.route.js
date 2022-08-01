234211480fceac9d0d58563fd355d4b5f2f8fdbf
bokhtiar <bokhtiartoshar1@gmail.com>
2022-08-02 00:36:42 +0600

    Admin authentication done using token grnerate, bcrypt hash protedted password and validation expressjs

--- src/router/adminRoute/auth/auth.route.js
+++ src/router/adminRoute/auth/auth.route.js
@@ -1,12 +1,14 @@
 const AdminAuthRoute = require('express').Router()
 const AuthController = require('../../../controller/adminController/authController')
-const {validateRequest, isRequestValidated} = require('../../../validator/adminAuth')
+const {validateRequest,
+     isRequestValidated,
+     loginValidateRquest, 
+     isLoginRequestValidated,
+    } = require('../../../validator/adminAuth')
 
     AdminAuthRoute.post('/register',validateRequest, isRequestValidated, AuthController.register)
-
-
     AdminAuthRoute.get('/list', AuthController.adminList)
-    AdminAuthRoute.post('/login', AuthController.Login)
+    AdminAuthRoute.post('/login', loginValidateRquest,isLoginRequestValidated, AuthController.Login)
     
 
 module.exports = AdminAuthRoute
--- src/validator/adminAuth.js
+++ src/validator/adminAuth.js
@@ -1,15 +1,31 @@
 const { check, validationResult } = require('express-validator');
 
+/*register validation*/
 exports.validateRequest = [
     check('name').notEmpty().withMessage('Name is required...!'),
     check('email').notEmpty().withMessage('Email is required...!'),
     check('email').isEmail().withMessage('Email Formate Eroor...!'),
 ]
 
+/*login validation*/
+exports.loginValidateRquest = [
+    check('email').notEmpty().withMessage('E-mail is required...!'),
+    check('email').isEmail().withMessage('Invalid Email Formater'),
+    check('password').notEmpty().withMessage('Password is required...!'),
+]
+
 exports.isRequestValidated = (req, res, next) => {
     const errors = validationResult(req)
     if(errors.array().length > 0){
         res.status(400).json({error: errors.array()[0].msg})
+    }
+    next()
+}
+
+exports.isLoginRequestValidated = (req, res, next) => {
+    const errors = validationResult(req)
+    if(errors.array().length > 0){
+        res.status(404).json({error: errors.array()[0].msg})
     }
     next()
 }
