d6cca81f0430f35bc7b71ad186e6d03a90b08412
bokhtiar <bokhtiartoshar1@gmail.com>
2022-08-02 00:22:34 +0600

    admin login with token genarate successfully maked done

--- src/controller/adminController/authController.js
+++ src/controller/adminController/authController.js
@@ -1,18 +1,19 @@
 const admins = require('../../models/admins')
+const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
 
 /*admin register */
-const register = async(req, res, next) => {
+const register = async (req, res, next) => {
     try {
         const {
             name,
             email,
             password,
             phone,
-        }= req.body
+        } = req.body
 
-        const account = await admins.findOne({email})
-        if(account){
+        const account = await admins.findOne({ email })
+        if (account) {
             res.status(401).json({
                 status: true,
                 message: "Account Already Exists..!"
@@ -20,11 +21,11 @@ const register = async(req, res, next) => {
         }
         /* Hash password */
         const hashPassword = await bcrypt.hash(password, 10)
-         
+
         const newAdmin = new admins({
             name,
             email,
-            password:hashPassword,
+            password: hashPassword,
             phone
         })
 
@@ -34,15 +35,15 @@ const register = async(req, res, next) => {
             status: true,
             message: "New Admin Created Successfully Done....!"
         })
-        
+
     } catch (error) {
         console.log(error);
         next(error)
-    } 
+    }
 }
 
 /*admin list */
-const adminList = async(req, res, next) => {
+const adminList = async (req, res, next) => {
     try {
         const results = await admins.find()
         res.status(201).json({
@@ -56,8 +57,50 @@ const adminList = async(req, res, next) => {
 }
 
 /*admin login*/
-const Login = (req, res, next)=>{
-    
+const Login = async (req, res, next) => {
+    try {
+        const {
+            email, password
+        } = req.body
+
+
+        /*exist email in admins table */
+        const account = await admins.findOne({ email })
+        if (!account) {
+            res.status(404).json({
+                status: true,
+                message: "Invalid Email or Password"
+            })
+        }
+
+        /*compare against accounts */
+        const result = await bcrypt.compare(password, account.password)
+        if (!result) {
+            return res.status(404).json({
+                status: false,
+                message: "Invalid email or password."
+            })
+        }
+
+        /* Generate JWT token */
+        const token = await jwt.sign(
+            {
+                id: account._id,
+                name: account.name,
+                role: account.role,
+                permissions: account.permissions,
+            }, process.env.JWT_SECRET, { expiresIn: '1d' }
+        )
+
+        res.status(201).json({
+            status: true,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        next()
+    }
 }
 
 
@@ -67,5 +110,5 @@ module.exports = {
     register,
     adminList,
     Login,
-    
+
 }
