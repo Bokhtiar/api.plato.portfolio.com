39e289143f0db15b0d900373231b5bfad4c2cc7f
bokhtiar <bokhtiartoshar1@gmail.com>
2022-08-04 12:27:33 +0600

    portfolio model created

---
+++ src/models/portfolio.js
@@ -0,0 +1,42 @@
+const {Schema, model} = require('mongoose')
+
+/*fild deaclear */
+const portfolioSchema = new Schema({
+    title: {
+        type: String,
+        trime: true,
+        require: true
+    },
+	body: { 
+        type: String,
+        trime: true,
+        require: true
+    },
+	image: {
+        type: String,
+        trime: true,
+        require: true
+    },
+	project_url:{
+        type: String,
+        trime: true,
+        require: false
+    },
+	client_name:{
+        type: String,
+        trime: true,
+        require: false
+    },
+	start_time: {
+        type: String,
+        trime: true,
+        require: false
+    }
+
+},{
+    timestamps: true
+})
+
+/*table name dacleare */
+const portfolios = model('portfolios', portfolioSchema)
+module.exports = portfolios
